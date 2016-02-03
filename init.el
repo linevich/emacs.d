b55fbfa388eda5c05ec3dcd390dae44a95f65bb1
Anton Linevich <linevich.net@gmail.com>
2016-02-03 19:38:03 +0200

    Migrate to use-package

--- init.el
+++ init.el
@@ -1,122 +1,163 @@
-;;; init.el --- my  .emacs config
+;;; init.el --- my .emacs config
+;; Anton Linevich <mail@linevich.net>
 ;;; Commentary:
 ;;; Code:
+
 
 ;;; Package settings
 (require 'package)
 (setq custom-file (locate-user-emacs-file "custom.el"))
 (load custom-file 'no-error 'no-message)
-(add-to-list 'load-path "~/.emacs.d/lisp/yasnippet")
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")
 			 ("org" . "http://orgmode.org/elpa/")))
 
-;; Package pipnning
-(when (boundp 'package-pinned-packages)
-  (setq package-pinned-packages
-	'((ergoemacs-mode . "gnu"))))
-
 (package-initialize)
 
-(defun install-required-packages (package-list)
-  (when (>= emacs-major-version 24)
-    (package-refresh-contents)
-    (mapc (lambda (package)
-            (unless (require package nil t)
-              (package-install package)))
-          package-list)))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Reuired pakcages
-(setq required-package-list '(yasnippet
-			      ergoemacs-mode
-			      sublime-themes
-			      helm-mode
-			      company
-			      spaceline-config
-			      rainbow-delimiters
-			      hgignore-mode
-			      gitignore-mode
-			      fill-column-indicator
-			      spacemacs-theme
-			      fill-column-indicator
-			      rainbow-mode
-			      multiple-cursors
-			      emmet-mode
-			      ace-jump-mode
-			      less-css-mode
-			      php-mode
-			      jinja2-mode
-			      autopair
-			      ))
+(setq use-package-verbose t)
+(require 'use-package)
 
-(install-required-packages required-package-list)
+(use-package auto-compile
+  :ensure t
+  :config (auto-compile-on-load-mode))
+(setq load-prefer-newer t)
 
-(setq ergoemacs-theme nil)
-(setq ergoemacs-keyboard-layout "us")
-(setq ergoemacs-message-level nil) ;; Disabling all debug messages.
-(ergoemacs-mode 1)
+(use-package ergoemacs
+  :pin gnu
+  :init (ergoemacs-mode 1)
+  :config (progn 
+	    (setq ergoemacs-theme nil)
+	    (setq ergoemacs-keyboard-layout "us")
+	    (setq ergoemacs-message-level nil) ;; Disabling all debug messages.
+	    (ergoemacs-mode 1)))
 
 ;;; General appearance settings.
-
-;; Setting color theme
-(load-theme 'spacemacs-dark t)
-
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; Disable menubar and toolbar.
+;; Disable menubar, toolbar and scrollbar.
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
-(spaceline-emacs-theme)
-(spaceline-helm-mode)
-(spaceline-toggle-column-on)
 
-(setq-default fill-column 100)
-(setq fci-rule-color "#3D3D3D")
-(setq fci-rule-column 100)
-(setq fci-rule-width 1)
-(setq fci-rule-character-color "#3D3D3D")
-(add-hook 'markdown-mode-hook 'fci-mode)
+
+;; Setting color theme
+(use-package spacemacs-theme
+  :ensure t
+  :init (load-theme 'spacemacs-dark t))
+
+(use-package spaceline
+  :ensure t
+  :init (progn
+	  (require 'spaceline-config)
+	  (spaceline-emacs-theme)
+	  (spaceline-helm-mode)
+	  (spaceline-toggle-column-on)))
+
+(use-package fill-column-indicator
+  :ensure t
+  :init (progn
+	  (setq-default fill-column 100)
+	  (setq fci-rule-color "#3D3D3D")
+	  (setq fci-rule-column 100)
+	  (setq fci-rule-width 1)
+	  (setq fci-rule-character-color "#3D3D3D")
+	  (add-hook 'markdown-mode-hook 'fci-mode)))
 
 
 ;;; Auto completion tools.
 
 ;; Helm M-x, switch buffer and other operations completion tool.
-(helm-mode 1)
+(use-package helm
+  :ensure t
+  :init (helm-mode 1)
+  )
+
 
 ;; Company mode - in-file completion engine.
-(global-company-mode t)
+(use-package company
+  :ensure t
+  :init (global-company-mode t))
 
 ;; Snippets
-(setq yas-snippet-dirs
-      '("~/.emacs.d/snippets"
-        "~/.emacs.d/lisp/yasnippet/snippets"
-	))
-(yas-global-mode t)
+
+(use-package yasnippet
+  :init
+  (progn 
+    (load-file "~/.emacs.d/lisp/yasnippet/yasnippet.el")
+    (yas-global-mode))
+  :config
+  (setq yas-snippet-dirs
+	'("~/.emacs.d/snippets"
+	  "~/.emacs.d/lisp/yasnippet/snippets")))
 
 ;; Parentheses highlight mode
-(rainbow-delimiters-mode t)
-(rainbow-mode t)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init (rainbow-delimiters-mode t))
+
+(use-package rainbow-mode
+  :ensure t
+  :init (rainbow-mode t))
+
 ;; Auto close parentheses
-(autopair-mode t)
+(use-package autopair
+  :ensure t
+  :config (autopair-mode t))
 
 ;; Flychek - checking syntax
-(global-flycheck-mode)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode)
+  )
 
 ;;; HTML & CSS/LESS settings
 
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook  'emmet-mode 'rainbow-mode)
-(add-hook 'less-css-mode-hook 'emmet-mode 'rainbow-mode)
+(use-package emmet-mode
+  :ensure t
+  :config (progn
+	    (add-hook 'sgml-mode-hook 'emmet-mode)
+	    (add-hook 'css-mode-hook  'emmet-mode)))
+
+(use-package rainbow-mode
+  :ensure t
+  :config (progn
+	    (add-hook 'css-mode-hook 'rainbow-mode)
+	    (add-hook 'less-css-mode-hook 'rainbow-mode)))
 
 ;;;  Keybindings
 (global-set-key (kbd "C-x p m") 'package-list-packages)
 (global-set-key (kbd "C-x C-f") 'fill-paragraph)
 
-(global-set-key (kbd "s-.") 'mc/mark-next-like-this)
-(global-set-key (kbd "s-,") 'mc/mark-previous-like-this)
+(use-package multiple-cursors
+  :ensure t
+  :config (progn 
+	    (global-set-key (kbd "s-.") 'mc/mark-next-like-this)
+	    (global-set-key (kbd "s-,") 'mc/mark-previous-like-this)))
+
+
+(use-package hgignore-mode :ensure t)
+(use-package gitignore-mode :ensure t)
+(use-package ace-jump-mode :ensure t)
+(use-package less-css-mode :ensure t)
+(use-package php-mode :ensure t)
+(use-package jinja2-mode :ensure t)
+(use-package autopair :ensure t)
+
+;; "Yes or no" to "y or n"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;; Backup settings
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 
 (provide 'init)
